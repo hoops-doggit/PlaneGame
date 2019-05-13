88908a1ab34440373ddc0f9ff21a29aafea72ee6
Unknown <jabcog@hotmail.com>
2019-05-13 22:01:55 +1200

    optimised floatyObject

--- Assets/_Scripts/FloatyObject.cs
+++ Assets/_Scripts/FloatyObject.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class FloatyObject : MonoBehaviour {
 
     public List<Transform> points;
-
     public float waterLineOffset = 1;
     public float rayProjectionWidth = 20;
     public float widthBetweenDepthRays = 10;
 
     private void Start()
     {
-        waterLineOffset = gameObject.GetComponent<Rigidbody>().mass;
+        float mass  = gameObject.GetComponent<Rigidbody>().mass;
+        if(mass <= 1)
+        {
+            waterLineOffset = mass;
+        } 
     }
 
 }
